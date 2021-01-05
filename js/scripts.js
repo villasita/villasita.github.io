ff0a553548e3aba081116bba2ce2227fcbb3bb9d
villasita <64595677+villasita@users.noreply.github.com>
2021-01-05 19:07:51 +0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").textContent = year;
+
+let modified = document.lastModified;
+document.getElementById("modified").textContent = modified;
